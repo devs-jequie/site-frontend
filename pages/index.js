212c1b877ebbb3e2df8327a6538243ced161aadb
Tulio Calil <tuliocalil@MacBook-Air-de-Tulio.local>
2022-10-05 09:23:24 -0300

    change text

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1>hello world</h1>
+      <h1>Ola Mundo</h1>
     </div>
-  )
+  );
 }
