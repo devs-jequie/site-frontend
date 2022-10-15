a49ae6867dafd3df60221370d7b9bdcd271e2c10
Flávia Correia <fllaviacorreia@outlook.com>
2022-10-15 11:57:43 -0300

    update login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
 import styles from "../styles/Home.module.css";
 
-import InputButton from "../components/inputs/Button";
-import Input from "../components/inputs/Input";
-import TextArea from "../components/inputs/TextArea";
-import InputUpload from "../components/inputs/InputUpload";
+import InputButton from "@/inputs/Button";
+import Input from "@/inputs/Input";
+import TextArea from "@/inputs/TextArea";
+import InputUpload from "@/inputs/InputUpload";
+
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Ola Mundo</h1>
+      <h3><Link href="/login"> Login</Link></h3>
+      
 
       <InputButton title="Teste" />
 
