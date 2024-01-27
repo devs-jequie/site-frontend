c879ed0cdcfda1b6dfaa3268e7a3ddce21c15a57
Tulio Calil <tuliocll@gmail.com>
2024-01-27 01:08:45 -0300

    Fix icon component

--- src/components/customIcon/index.tsx
+++ src/components/customIcon/index.tsx
@@ -2,5 +2,7 @@
 import * as icons from "react-icons/bs";
 
 export const Icon = ({ iconName, ...props }) => {
-  return <div>oi</div>;
+  const BootstrapIcon = icons[`Bs${iconName}`];
+
+  return BootstrapIcon ? <BootstrapIcon {...props} /> : null;
 };
